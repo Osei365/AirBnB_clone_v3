9e13fdfe3a5c6491287593f1ac10f4b6a4394bfb
Osei <williamsabodunrin@gmail.com>
2023-10-28 22:11:17 +0000

    Update app

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,10 +4,12 @@
 from os import getenv
 from flask import Flask
 from models import storage
+from flask_cors import CORS
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def handle_teardown(error):
