75d62f2ec24d5cd2c69b298b05be076fd200aed0
Osei <williamsabodunrin@gmail.com>
2023-10-28 16:29:59 -0700

    api creation

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-"""status of API."""
-
-from os import getenv
-from flask import Flask
+""" creating an app"""
+from flask import Flask, make_response, jsonify
 from models import storage
-from flask_cors import CORS
+import os
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
-def handle_teardown(error):
+def teardown_appcontext(error):
+    """close method close()"""
     storage.close()
 
-
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(getenv('HBNB_API_PORT', 5000))
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
