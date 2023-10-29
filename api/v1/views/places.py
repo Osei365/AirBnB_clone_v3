2009a4b5ad808a075cab02387fda5fdc2399f505
Osei <williamsabodunrin@gmail.com>
2023-10-29 16:29:48 +0000

    Update places

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -3,13 +3,15 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
 from models import storage
-from models.engine.db_storage import classes
+from models.place import Place
+from models.user import User
+from models.city import City
 
 
 @app_views.route('/cities/<city_id>/places',
                  methods=['GET', 'POST'], strict_slashes=False)
 def get_places_by_cities(city_id):
-    city = storage.get(classes['City'], city_id)
+    city = storage.get(City, city_id)
     if city is None:
         abort(404)
     if request.method == 'POST':
@@ -18,12 +20,12 @@ def get_places_by_cities(city_id):
             abort(400, 'Not a JSON')
         if 'user_id' not in json_dic:
             abort(400, 'Missing user_id')
-        user = storage.get(classes['User'], json_dic.get('user_id'))
+        user = storage.get(User, json_dic.get('user_id'))
         if user is None:
             abort(404)
         if 'name' not in json_dic:
             abort(400, 'Missing name')
-        place = classes['Place'](**json_dic)
+        place = Place(**json_dic)
         place.city_id = city_id
         place.save()
         return make_response(jsonify(place.to_dict()), 201)
@@ -34,7 +36,7 @@ def get_places_by_cities(city_id):
 @app_views.route('/places/<place_id>',
                  methods=['GET'], strict_slashes=False)
 def get_place(place_id):
-    obj = storage.get(classes['Place'], place_id)
+    obj = storage.get(Place, place_id)
     if obj is not None:
         return jsonify(obj.to_dict())
     abort(404)
@@ -43,7 +45,7 @@ def get_place(place_id):
 @app_views.route('/places/<place_id>',
                  methods=['DELETE'], strict_slashes=False)
 def delete_place(place_id):
-    obj = storage.get(classes['Place'], place_id)
+    obj = storage.get(Place, place_id)
     if obj is not None:
         obj.delete()
         storage.save()
@@ -54,7 +56,7 @@ def delete_place(place_id):
 @app_views.route('/places/<place_id>',
                  methods=['PUT'], strict_slashes=False)
 def put_place(place_id):
-    obj = storage.get(classes['Place'], place_id)
+    obj = storage.get(Place, place_id)
     if obj is None:
         abort(404)
     json_dict = request.get_json()
