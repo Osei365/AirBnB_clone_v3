7973e214c96aa793fdce191dc0ed23f7bd42ac14
Osei <williamsabodunrin@gmail.com>
2023-10-28 17:09:52 -0700

    404 error

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,6 +14,12 @@ def teardown_appcontext(error):
     """close method close()"""
     storage.close()
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(os.getenv("HBNB_API_PORT", 5000))
