c708cb26d0672c56437a3258da3c903ce8b7e680
Osei <williamsabodunrin@gmail.com>
2023-10-28 20:10:04 -0700

    cities api

---
+++ api/v1/views/cities.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""returns a json string"""
+from api.v1.views import app_views
+from flask import jsonify, abort, request, make_response
+from models import storage
+from models.engine.db_storage import classes
+
+
+@app_views.route('/states/<state_id>/cities',
+                 methods=['GET', 'POST'], strict_slashes=False)
+def get_cities_by_states(state_id):
+    state = storage.get(classes['State'], state_id)
+    if state is None:
+        abort(404)
+    if request.method == 'POST':
+        json_dic = request.get_json()
+        if json_dic is None:
+            abort(400, 'Not a JSON')
+        if 'name' not in json_dic:
+            abort(400, 'Missing name')
+        city = classes['City'](**json_dic)
+        city.state_id = state_id
+        city.save()
+        return make_response(jsonify(city.to_dict()), 201)
+    new_objs = [obj.to_dict() for obj in state.cities]
+    return jsonify(new_objs)
+
+
+@app_views.route('/cities/<city_id>',
+                 methods=['GET'], strict_slashes=False)
+def get_city(city_id):
+    obj = storage.get(classes['City'], city_id)
+    if obj is not None:
+        return jsonify(obj.to_dict())
+    abort(404)
+
+
+@app_views.route('/cities/<city_id>',
+                 methods=['DELETE'], strict_slashes=False)
+def delete_city(city_id):
+    obj = storage.get(classes['City'], city_id)
+    if obj is not None:
+        obj.delete()
+        storage.save()
+        return jsonify({})
+    abort(404)
+
+
+@app_views.route('/cities/<city_id>',
+                 methods=['PUT'], strict_slashes=False)
+def put_city(city_id):
+    obj = storage.get(classes['City'], city_id)
+    if obj is None:
+        abort(404)
+    json_dict = request.get_json()
+    if json_dict is None:
+        abort(400, 'Not a JSON')
+    for key, value in json_dict.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(obj, key, value)
+    obj.save()
+    return make_response(jsonify(obj.to_dict()), 200)
