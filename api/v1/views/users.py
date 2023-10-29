801d003a4e1d573671ccb0fdca41b867010e50dd
Osei <williamsabodunrin@gmail.com>
2023-10-29 15:34:12 +0000

    Update users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -3,6 +3,7 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
 from models import storage
+from models.user import User
 from models.engine.db_storage import classes
 
 
