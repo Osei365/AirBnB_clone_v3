ff6c9ce4c64770f6f3d4d4f1a8be6581f79928c1
Osei <williamsabodunrin@gmail.com>
2023-10-30 10:26:26 +0000

    Update places_amenities

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -27,13 +27,15 @@ def get_amenities_by_places(place_id):
 def delete_amenities_by_place(place_id, amenity_id):
     place = storage.get(Place, place_id)
     amenity = storage.get(Amenity, amenity_id)
-    if not place or not amenity:
-        abort(404)
-    if amenity not in place.amenities:
+    if place is None or amenity is None:
         abort(404)
     if models.storage_t == 'db':
+        if amenity not in place.amenities:
+            abort(404)
         place.amenities.remove(amenity)
     else:
+        if amenity_id not in place.amenity_ids:
+            abort(404)
         place.amenity_ids.remove(amenity_id)
     storage.save()
     return jsonify({})
@@ -46,11 +48,13 @@ def post_amenities_by_place(place_id, amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if not place or not amenity:
         abort(404)
-    if amenity in place.amenities:
-        return jsonify(amenity.to_dict())
     if models.storage_t == 'db':
+        if amenity in place.amenities:
+           return jsonify(amenity.to_dict())
         place.amenities.append(amenity)
     else:
+        if amenity_id in place.amenity_ids:
+            return jsonify(amenity.to_dict())
         place.amenity_ids.append(amenity_id)
     storage.save()
     return make_response(jsonify(amenity.to_dict()), 201)
