33d4bd948a1b6a9128b66a6dd43ece2682d802eb
Osei <williamsabodunrin@gmail.com>
2023-10-29 17:59:40 -0700

    security

--- models/user.py
+++ models/user.py
@@ -32,7 +32,7 @@ class User(BaseModel, Base):
             m.update(bytes(kwargs['password']))
             kwargs['password'] = m.hexdigest()
         super().__init__(*args, **kwargs)
-        
+
     def __setattr__(self, key, value):
         if key == 'password':
             m = hashlib.md5()
