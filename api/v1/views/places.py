0fe3d9ab54545810400dfe019a4ff358f7bba7ca
Osei <williamsabodunrin@gmail.com>
2023-10-30 06:13:17 -0700

    add place search

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -3,9 +3,11 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
 from models import storage
+from models.state import State
 from models.place import Place
 from models.user import User
 from models.city import City
+from models.amenity import Amenity
 
 
 @app_views.route('/cities/<city_id>/places',
@@ -67,3 +69,39 @@ def put_place(place_id):
             setattr(obj, key, value)
     obj.save()
     return make_response(jsonify(obj.to_dict()), 200)
+
+
+@app_views.route('/places_search', methods=['POST'], strict_slashes=False)
+def place_search():
+    json_dict = request.get_json()
+    if json_dict is None:
+        abort(400, 'Not a JSON')
+    result = []
+    all_keys = [len(arr) == 0 for arr in json_dict.values()]
+    if len(json_dict) == 0 or all(all_keys):
+        places = storage.all(Place)
+        result.extend([obj.to_dict() for obj in places.values()])
+    states_list = json_dict.get('states')
+    city_lists = json_dict.get('cities')
+    if city_lists and len(city_lists) > 0:
+        for city_id in city_lists:
+            city = storage.get(City, city_id)
+            if city:
+                result.extend([place.to_dict() for place in city.places])
+    if states_list and len(states_list) > 0:
+        for state_id in states_list:
+            state = storage.get(State, state_id)
+            if state:
+                for c in state.cities:
+                    if city_lists and c.id not in city_lists:
+                        result.extend([place.to_dict() for place in c.places])
+    amenity_lists = json_dict.get('amenities')
+    new_result = result.copy()
+    if amenity_lists and len(amenity_lists) > 0:
+        for place_dict in new_result:
+            place = storage.get(Place, place_dict['id'])
+            for amenity_id in amenity_lists:
+                amenity = storage.get(Amenity, aemnity_id)
+                if amenity not in place.amenities:
+                    result.remove(place_dict)
+    return jsonify(result)
